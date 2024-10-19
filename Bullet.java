e2eb477cc734f6a8f0ffe288459bf04d8bb58ab9
Стрельцов Кирилл Евгеньевич <kirill.streltsov.2023@mail.ru>
2024-10-19 16:42:23 +0700

    Импорт классов Entity и Bullet

---
+++ Bullet.java
@@ -0,0 +1,65 @@
+package DOM;
+
+public class Bullet extends Entity{
+
+    private double finalCoordX; //конечная координата пули по X
+    private double finalCoordY; //конечная координата пул по Y
+
+
+    public Bullet(double coord_X, double coord_Y, double final_coord_X, double final_coord_Y, int entity_Damage, double entity_Speed){
+        this.coordX = coord_X;
+        this.coordY = coord_Y;
+        this.finalCoordX = final_coord_X;
+        this.finalCoordY = final_coord_Y;
+        this.speed = entity_Speed;
+        this.damage = entity_Damage;
+    }
+
+    public Bullet(){
+        this.coordX = -1;
+        this.coordY = -1;
+        this.finalCoordX = -1;
+        this.finalCoordY = -1;
+        this.speed = 0.2;
+        this.damage = 50;
+    }
+
+    //получение координат точки назначения пули
+    public int getBulletCoords(double[] final_coord){
+        final_coord[0] = coordX;
+        final_coord[1] = coordY;
+        return 0;
+    }
+
+    public int getBulletCoords(int[] final_coord){
+        final_coord[0] = (int) coordX;
+        final_coord[1] = (int) coordY;
+        return 0;
+    }
+
+    //движение пули
+    public int bulletMovment(){
+
+        double deltaX = finalCoordX - coordX;
+        double deltaY = finalCoordY - coordY;
+
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            // Движение по оси X
+            if (deltaX < 0) {
+                this.entityStep(CardinalDirections.North);
+            } else {
+                this.entityStep(CardinalDirections.South);
+            }
+        } else {
+            // Движение по оси Y
+            if (deltaY < 0) {
+                this.entityStep(CardinalDirections.West);
+            } else {
+                this.entityStep(CardinalDirections.East);
+            }
+        }
+        return  0;
+    }
+
+
+}
---
+++ Entity.java
@@ -0,0 +1,86 @@
+package DOM;
+
+enum CardinalDirections {
+    North, East, South, West
+};
+
+public class Entity {
+    protected double coordX;             //координата по X
+    protected double coordY;             //координата по Y
+    protected int hitPoints;             //очки здоровья
+    protected int damage;                 //урон наносимый
+    protected double speed;               //скорость
+
+    public Entity(double coord_X, double coord_Y, double entity_Speed, int hit_Points, int entity_Damage) {
+        this.coordX = coord_X;
+        this.coordY = coord_Y;
+        this.speed = entity_Speed;
+        this.hitPoints = hit_Points;
+        this.damage = entity_Damage;
+    }
+
+    public Entity() {
+        this.coordX = 8;
+        this.coordY = 1;
+        this.hitPoints = 100;
+        this.speed = 1;
+        this.damage = 50;
+    }
+
+    public int getEntityCoord(double[] coord) {
+        if (hitPoints > 0) {
+            coord[0] = coordX;
+            coord[1] = coordY;
+            return 0;
+        }
+        return 1;
+    }
+
+    public int getEntityCoord(int[] coord) {
+        if (hitPoints > 0) {
+            coord[0] = (int) coordX;
+            coord[1] = (int) coordY;
+            return 0;
+        }
+        return  1;
+    }
+
+    public int getEntityDamage() {
+        return damage;
+    }
+
+    public int getEntityHitPoints() {
+        return hitPoints;
+    }
+
+    // Метод атаки сущности
+    public int attackEntity(int entity_Damage) {
+        if (hitPoints > 0) {
+            hitPoints -= entity_Damage;
+            return 0; // Успешная атака
+        } else {
+            return 1; // Сущность не может быть атакована, так как здоровье равно или меньше 0
+        }
+    }
+
+    public int entityStep(CardinalDirections step_Direction) {
+        // Здесь вы можете определить логику перемещения в зависимости от направления
+        if (hitPoints > 0)
+        {
+            int i = 0;
+            switch (step_Direction)
+            {
+                case North: coordX -= speed; break;
+                case East: coordY += speed; break;
+                case South: coordX += speed; break;
+                case West: coordY -= speed; break;
+                default: i = 1;
+            }
+
+            return i;
+        }
+        return 2;
+    }
+
+};
+
